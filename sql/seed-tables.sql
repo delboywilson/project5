ac0b209e915d928736dd702f2577f9b0e45b41bb
Julia Sharnina <>
2021-03-09 11:37:41 +1000

    seeds

--- sql/seed-tables.sql
+++ sql/seed-tables.sql
@@ -1,3 +1,15 @@
--- seed a dozen users
-
-
+--seed users table
+INSERT INTO users(username, first_name, last_name, email, password)
+VALUES
+('Hoc', 'Allison','Hocus Pocus','hokus@pokus.com','Hello1'),
+('Alm', 'Lorraine','Almaden','Lorrain@almaden.com','Hello1'),
+('Heu', 'Alucard','Van Heusen','Alucard@heusen.com','Hello1'),
+('Ama','Liv','Amara','Liv@amara.com','Hello1'),
+('Amb','Princess','Amber','Princess@amber.com','Hello1'),
+('Mac','Andi','Mack','Andi@mack.com','Hello1'),
+('Ame','Captain','Amelia','Captain@amelia.com','Hello1'),
+('Duc','Amelia','Duckworth','Amelia@duckworth.com','Hello1'),
+('Arc','Amicus','Arcane','Amicus@arcane.com','Hello1'),
+('Bli','Amity','Blight','Amity@blight.com','Hello1'),
+('Cor','Ammand','the Corsair','Ammand@corsair.com','Hello1'),
+('And','Captain','Anderson','Captain@anderson.com','Hello1');
