89632cf8d117fb8ec73f03138e79e0769aeb2e5b
Julia Sharnina <>
2021-03-09 10:37:07 +1000

    passport auth

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash')
+
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
+
+
+const users = [
+  { id: 1, name: 'Alex', email: 'alex@gmail.com', password: 'Hello1' },
+  { id: 2, name: 'Mila', email: 'mila@gmail.com', password: 'Hello1' },
+  { id: 3, name: 'Milo', email: 'milo@gmail.com', password: 'Hello1' }
+]
 
 router.get("/", (req, res) => {
   res.render("pages/login");
 });
+
+router.post("/login", passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
 
 module.exports = router;
